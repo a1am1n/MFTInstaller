3cdcba21abeb76de19128cdef8ff0ff8f348a27c
Nirav <niravnvk@gmail.com>
2015-06-08 11:49:44 +0530

    rgyhj

--- app/src/main/java/com/app/mftinsatller/MainActivity.java
+++ app/src/main/java/com/app/mftinsatller/MainActivity.java
@@ -1,17 +1,80 @@
 package com.app.mftinsatller;
 
+import android.content.Context;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.twotoasters.jazzylistview.JazzyListView;
+import com.twotoasters.jazzylistview.effects.CurlEffect;
 
 
 public class MainActivity extends ActionBarActivity {
-
+    JazzyListView applistView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        applistView = (JazzyListView)findViewById(R.id.applistView);
+
+        applistView.setTransitionEffect(new CurlEffect());
+
+        ListAdapter adp = new ListAdapter(MainActivity.this);
+        applistView.setAdapter(adp);
+        applistView.setAdapter(adp);
+
+    }
+
+
+    class ListAdapter extends BaseAdapter {
+        LayoutInflater layoutInflator;
+        private Context ctx;
+
+        public ListAdapter(Context ctx){
+            this.ctx = ctx;
+
+        }
+
+
+        @Override
+        public int getCount() {
+            return 10;
+        }
+
+
+        @Override
+        public Object getItem(int i) {
+            return 0;
+        }
+
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View convertView, ViewGroup viewGroup) {
+            layoutInflator = (LayoutInflater) ctx.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View view = convertView;
+            view = layoutInflator.inflate(R.layout.list_item, viewGroup, false);
+
+            TextView txtAppName  = (TextView)findViewById(R.id.txtAppName);
+            TextView txtDate  = (TextView)findViewById(R.id.txtDate);
+            ImageView imgDownload = (ImageView)findViewById(R.id.imgDownload);
+
+            return view;
+        }
+
+
     }
 
     @Override
@@ -20,6 +83,8 @@ public class MainActivity extends ActionBarActivity {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
+
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
