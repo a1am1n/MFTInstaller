ac63602b7e9d0cf59229b3eec4b167b2fa3cdc0a
Krishna <softeng.krishna@gmail.com>
2015-07-05 22:05:21 +0530

    dwfsdfbcnv

--- app/src/main/java/com/app/mftinsatller/Aboutus.java
+++ app/src/main/java/com/app/mftinsatller/Aboutus.java
@@ -6,25 +6,27 @@ import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 
 public class Aboutus extends ActionBarActivity {
 
-    TextView txtvpn,youtube,txtContact;
+    Button txtvpn,youtube,txtContact,txtinst;
 
     String CONTACT_URL = "http://mtlfreetv.com/contact-us/contact-us-form";
     String YOUTUBE_URL = "https://www.youtube.com/channel/UC389S4_2Yt9cei1qNDwmBrA?sub_confirmation=1";
     String VPN_URL = "https://www.bit.ly/mftvpn";
-
+    String INST_URL = "http://www.sn.im/mftinstaller";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.aboutus);
 
-        txtContact = (TextView)findViewById(R.id.txtContact);
-        youtube = (TextView)findViewById(R.id.youtube);
-        txtvpn = (TextView)findViewById(R.id.txtvpn);
+        txtContact = (Button)findViewById(R.id.txtContact);
+        youtube = (Button)findViewById(R.id.youtube);
+        txtvpn = (Button)findViewById(R.id.txtvpn);
+        txtinst = (Button)findViewById(R.id.txtinst);
 
 
         txtContact.setOnClickListener(new View.OnClickListener() {
@@ -51,6 +53,15 @@ public class Aboutus extends ActionBarActivity {
             public void onClick(View view) {
                 Intent i = new Intent(Intent.ACTION_VIEW);
                 i.setData(Uri.parse(VPN_URL));
+                startActivity(i);
+            }
+        });
+
+        txtinst.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(INST_URL));
                 startActivity(i);
             }
         });
